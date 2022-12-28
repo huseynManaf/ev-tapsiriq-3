7c07a827bad1108172d04b41d0b1684649e72be3
huseynManaf <huseynim@code.edu.az>
2022-12-28 01:05:22 -0800

    init

---
+++ tapsiriq 3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace tapsiriq_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ededi daxil edin");
+            int.Parse(Console.ReadLine());
+            int a, b, c;
+            a = 16538;
+            b = 4;
+                c = 2;
+            Console.WriteLine($"{b}16538{c}");
+            
+        }
+    }
+}
